5bbda5fc7342ac8034d6d575015bc53c1c0bf11f
otodreas <159846818+otodreas@users.noreply.github.com>
2025-11-06 01:11:32 +0100

    finished py files

--- knn/knntest.py
+++ knn/knntest.py
@@ -2,7 +2,6 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 
 from knn import KNN
 
@@ -10,10 +9,10 @@ from knn import KNN
 # Load data
 iris = datasets.load_iris()
 X, y = iris.data, iris.target  # X: values. y: classes
-# Split data
+# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Create classifier
+# Create classifier where k=3
 clf = KNN(k=3)
 # Fit data (in this case, this step is only necessary for assigning variables)
 clf.fit(X_train, y_train)
@@ -21,11 +20,15 @@ clf.fit(X_train, y_train)
 predictions = clf.predict(X_test)
 
 # Check how many of our predictions are correctly classified
+# Correct classifications are stored in y_test
 acc = np.sum(predictions == y_test) / len(y_test)
-print(acc)
 
-# Plot
-fig, ax = plt.subplots()
-ax.scatter(X_test[:, 2], X_test[:, 3], c=y_test)
-fig.suptitle('knn predictions (color = predicted class)')
-plt.show()
+# Print summary
+print(f"True class:      {y_test}")
+print(f"Predicted class: {predictions}")
+print(f"Accuracy of classifier: {round(acc*100, 2)}%")
+
+
+# # Get the distribution of each variable
+# for i, col in enumerate(range(X_train.shape[1])):
+#     dist = X_train[:, col]
